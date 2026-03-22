8bd88c080cec2fef09aa2ca756a7fe978479d252
Vinicius Ferreira <viniciusaugusto1997@gmail.com>
2026-03-22 12:19:59 -0300

    fix: ajuste porta para deploy render

--- server.js
+++ server.js
@@ -6,8 +6,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, function () {
   console.log(`Rodando em http://localhost:${PORT}`);
